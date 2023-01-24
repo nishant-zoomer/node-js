48d9ef3dae5f9b386d01c808210cd3ac660656a0
harshuuu18 <probookh118@gmail.com>
2023-01-24 22:07:37 +0530

    ecosystem added

--- prod-ecosystem.config.js
+++ prod-ecosystem.config.js
@@ -1,28 +1,14 @@
 module.exports = {
   apps: [
 
-    {
-      name: "fr_admin",
-      script: "dist/admin/app.js",
 
-    },
     {
-      name: "fr_app",
+      name: "duffer_app",
       script: "dist/users/app.js",
 
     },
 
-    {
-      name: "fr_public",
-      script: "dist/public.js",
 
-    },
-
-    {
-      name: "fr_socket",
-      script: "dist/socket/index.js",
-
-    },
 
   ],
 };
