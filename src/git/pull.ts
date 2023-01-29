2f437122b1c64227decc084bf54882e02e843b33
harshuuu18 <probookh118@gmail.com>
2023-01-29 12:50:39 +0530

    order list api added

--- src/git/pull.ts
+++ src/git/pull.ts
@@ -1,10 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
 import simplegit from "simple-git";
 import chilprocess from "child_process";
 const git = simplegit({ config: [] });
 
 (async () => {
 	const USER = "harshuuu18";
-	const PASS = "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";
+	const PASS = process.env.GIT_USER_TOKEN || "";
 	const REPO = "github.com/nishant-zoomer/node-js";
 
 	const remote = `https://${USER}:${PASS}@${REPO}`;
