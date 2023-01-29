177d4e79a77266ef20d6bcc0498d47c7f22fe74d
harshuuu18 <probookh118@gmail.com>
2023-01-29 15:05:52 +0530

    order list api added

--- src/users/controllers/product.ts
+++ src/users/controllers/product.ts
@@ -35,6 +35,11 @@ export default {
 
 		let products = await Product.aggregate([
 			{
+				$match: {
+					user: {
+						$ne: req.user?._id,
+					},
+				},
 				$geoNear: {
 					near: {
 						type: "Point",
